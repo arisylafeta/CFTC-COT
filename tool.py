cb4189675ff1eccf468a4f929baa44b211e0c36d
Arianit Sylafeta <ari.sylafeta@gmail.com>
2023-03-29 17:28:53 +0200

    First run

--- tool.py
+++ tool.py
@@ -1,25 +1,45 @@
-#!/usr/bin/env python
-
-# make sure to install these packages before running:
-# pip install pandas
-# pip install sodapy
-
+import requests
+import streamlit as st
+import numpy as np
 import pandas as pd
 from sodapy import Socrata
+import os
+import base64
 
-# Unauthenticated client only works with public data sets. Note 'None'
-# in place of application token, and no username or password:
-client = Socrata("publicreporting.cftc.gov", None)
+#Fetch the data from CFTC Website
+def FetchData(date, commodity):
+    client = Socrata("publicreporting.cftc.gov", None)
+    results = client.get(
+                        "6dca-aqww",
+                        limit=100,
+                        select="market_and_exchange_names AS name, commodity_name AS type, report_date_as_yyyy_mm_dd AS date, noncomm_positions_long_all AS long, noncomm_positions_short_all AS short",
+                        where="commodity_name IN ('GOLD', 'SILVER')",
+                        order = "report_date_as_yyyy_mm_dd DESC" )
+    results_df = pd.DataFrame.from_records(results)
+    return results_df
 
-# Example authenticated client (needed for non-public datasets):
-# client = Socrata(publicreporting.cftc.gov,
-#                  MyAppToken,
-#                  username="user@example.com",
-#                  password="AFakePassword")
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+img = get_img_as_base64("img.png")
 
-# First 2000 results, returned as JSON from API / converted to Python list of
-# dictionaries by sodapy.
-results = client.get("6dca-aqww", limit=2000)
+#App Interface
 
-# Convert to pandas DataFrame
-results_df = pd.DataFrame.from_records(results)
+page_bg_img =  """
+<style>
+[data-testid="stAppViewContainer"] {
+background-image: url("https://img.freepik.com/premium-vector/economy-finance-concept-financial-business-statistics-stock-market-candlesticks-bar-chart_120819-2230.jpg?w=1060");
+background-size: cover;
+}
+[data-testid="stHeader"] {
+    background-color: rgba(0,0,0,0);
+}
+[data-testid="stToolbar"] {
+    right: 2rem;
+}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+st.markdown('A title with _italics_ :red[colors] and emojis :sunglasses:')
+
