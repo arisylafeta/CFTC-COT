e38e5a601b954a61443088cbefbc4c37076ae129
Arianit Sylafeta <ari.sylafeta@gmail.com>
2023-03-30 17:54:22 +0200

    Third commit

--- tool.py
+++ tool.py
@@ -5,31 +5,70 @@ import pandas as pd
 from sodapy import Socrata
 import os
 import base64
+from datetime import datetime
+import altair as alt
+import plotly.express as px
+
+def comTrans(selection):
+    match selection:
+        case 'SP500':
+            id = '13874+'
+        case 'NDX100':
+            id = '20974+'
+        case 'GOLD':
+            id = '088691'
+        case 'SILVER':
+            id = '084691'
+        case 'USOIL':
+            id = '06765I'
+        case 'EURUSD':
+            id = '099741'
+        case 'GBPUSD':
+            id = '096742'
+        case 'JPYUSD':
+            id = '097741'
+        case 'NZDUSD':
+            id = '112741'
+        case 'AUDUSD':
+            id = '232741'
+        case 'CADUSD':
+            id = '090741'
+        case 'CHFUSD':
+            id = '092741'
+    return id
 
 #Fetch the data from CFTC Website
-def FetchData(date, commodity):
+def FetchData(commodity):
     client = Socrata("publicreporting.cftc.gov", None)
     results = client.get(
                         "6dca-aqww",
                         limit=100,
-                        select="market_and_exchange_names AS name, commodity_name AS type, report_date_as_yyyy_mm_dd AS date, noncomm_positions_long_all AS long, noncomm_positions_short_all AS short",
-                        where="commodity_name IN ('GOLD', 'SILVER')",
+                        select='''contract_market_name AS name,
+                                cftc_contract_market_code as id,  
+                                commodity_name AS type, 
+                                report_date_as_yyyy_mm_dd AS date, 
+                                open_interest_all as open_interest,
+                                change_in_open_interest_all as interest_change, 
+                                noncomm_positions_long_all AS long, 
+                                noncomm_positions_short_all AS short, 
+                                comm_positions_long_all AS comm_long, 
+                                comm_positions_short_all AS comm_short,
+                                change_in_noncomm_long_all as long_change,
+                                change_in_noncomm_short_all as short_change, 
+                                change_in_comm_long_all as comm_long_change,
+                                change_in_comm_short_all as comm_short_change''',
+                        where=f"cftc_contract_market_code = '{commodity}'",
                         order = "report_date_as_yyyy_mm_dd DESC" )
     results_df = pd.DataFrame.from_records(results)
     return results_df
 
-def get_img_as_base64(file):
-    with open(file, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-img = get_img_as_base64("img.png")
 
 #App Interface
 
 page_bg_img =  """
 <style>
 [data-testid="stAppViewContainer"] {
-background-image: url("https://img.freepik.com/premium-vector/economy-finance-concept-financial-business-statistics-stock-market-candlesticks-bar-chart_120819-2230.jpg?w=1060");
+background-image: url("https://i.imgur.com/y8o7Vq3.jpg");
 background-size: cover;
 }
 [data-testid="stHeader"] {
@@ -41,5 +80,25 @@ background-size: cover;
 </style>
 """
 st.markdown(page_bg_img, unsafe_allow_html=True)
-st.markdown('A title with _italics_ :red[colors] and emojis :sunglasses:')
+st.title('Welcome to Trading Pal :chart:')
+st.text('''Trading Pal is a tool that helps you analyze the COT Report. 
+Select the commodity and timespan and check what institutions are doing.''')
+st.write("")
+col1, col2 = st.columns(2)
+
+with col1:
+    commodity = st.selectbox("1. Select your commodity", ('SP500', 'NDX100', 'GOLD', 'SILVER', 'USOIL', 'EURUSD', 'GBPUSD', 'JPYUSD', 'CADUSD', 'AUDUSD' ,'NZDUSD',  'CHFUSD'))
+with col2:
+    st.write('')
+    
+if st.button("Let's get it"):
+        commodityID = comTrans(commodity)
+        df = FetchData(commodityID)
+        df['date'] = df['date'].apply(lambda x: x[:-13])
+        showdata = df[['date', 'long', 'short', 'long_change', 'short_change']]
+        st.dataframe(showdata)
+
+
+        
+
 
